7bfee243e6fd670ef1f886f3d3e16b2161c64864
keery <mon-compte1131@live.fr>
2015-02-01 18:51:46 +0100

    Calendrier

---
+++ calendrier.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" /> 
+		<!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+				<title>Calendrier</title>
+			<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+		<TABLE> 
+			<CAPTION>Calendrier</CAPTION> 
+			  <TR> 
+				 <TH>Lunae</TH> 
+				 <TH>Martis</TH> 
+				 <TH>Mercurii</TH> 
+				 <TH>Jovis</TH> 
+				 <TH>Veneris</TH> 
+				 <TH>Saturni</TH> 
+				 <TH>Dominicus</TH> 
+			  </TR>
+
+			  <TR> 
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+			  </TR> 
+
+			   <TR> 
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+			  </TR> 
+			  
+			   <TR> 
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+			  </TR> 
+
+			   <TR> 
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+			  </TR> 
+
+			   <TR> 
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+				<TD></TD>
+			  </TR> 
+		</TABLE> 
+	</body>
+
+</html>
