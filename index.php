56d800c607041463a589cbe18f84fac0ed29acb5
navec <navecbatchi@gmail.com>
2015-02-04 03:07:28 +0100

    nettoyage du php

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 	// Je déclare mes tableaux ici
 	$mois = array("Januarius", "Februarius", "Martius", "Aprilis", "Maius", "Junius", "Julius", "Augustus", "September", "October", "November", "December");
 	$nomJour = array("","Lunae", "Martis", "Mercurii", "Jovis", "Veneris", "Saturni", "Dominicus");
-	$numeroJour = array("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", " ");
 	// Variable qui me servira à afficher les numéros de mes jours
-	$j = 0;
+	$j = 1;
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,10 +34,10 @@
 				echo "<td></td>";
 			}
 			// Tant que $j est inférieur au nombre de jours du mois
-			while($j  < $number) { 
+			while($j  <= $number) { 
 				// Je ferme et ouvre mon tr quand b est différent de zéro et qu'il est multiple de 7
 				if($b%7 == 0 && $b != 0) echo "</tr><tr>";
-				echo "<td>".$numeroJour[$j]."</td>";
+				echo "<td>".$j."</td>";
 				// j'incrémente le $b pour le tr
 				$b++;
 				// j'incrémente le $j pour les jours et pour que la condition du while soit vrai.
