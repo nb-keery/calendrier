8af1455927ab3016e0ed2fcaa933aa79f06b16a7
keery <mon-compte1131@live.fr>
2015-02-19 16:21:46 +0100

    Calendrier palier 1 et 2

--- calendrier.php
+++ calendrier.php
@@ -12,16 +12,36 @@
 	<body>
 
 	<?php $mois = array('Januarius', 'Februarius', 'Martius', 'Aprilis', 'Maius', 'Junius', 'Augustus', 'September', 'October', 'November', 'December');
-		  $semaine = array('Lunae', 'Martis', 'Mercurii', 'Jovis', 'Veneris', 'Saturni', 'Dominicus'); ?>
+		  $semaine = array('Lunae', 'Martis', 'Mercurii', 'Jovis', 'Veneris', 'Saturni', 'Dominicus'); 
+			function chif_rome($num)
+			{
+			  //I V X  L  C   D   M
+			  //1 5 10 50 100 500 1k
+			  $rome =array("","I","II","III","IV","V","VI","VII","VIII","IX");
+			  $rome2=array("","X","XX","XXX","XL","L","LX","LXX","LXXX","XC");
+			  $rome3=array("","C","CC","CCC","CD","D","DC","DCC","DCCC","CM");
+			  $rome4=array("","M","MM","MMM","IVM","VM","VIM","VIIM","VIIIM","IXM");
+			  $str=$rome[$num%10];
+			  $num-=($num%10);
+			  $num/=10;
+			  $str=$rome2[$num%10].$str;
+			  $num-=($num%10);
+			  $num/=10;
+			  $str=$rome3[$num%10].$str;
+			  $num-=($num%10);
+			  $num/=10;
+			  $str=$rome4[$num%10].$str;
+			  return $str;
+			}?>
 
 
 	<h1>Calendrier</h1> 
 
-		<span><?php echo date('F Y'); ?></span>
+		<span><img src="gauche.png"><?php echo date('F Y'); ?><img src="droite.png"></span>
 		
 		<TABLE> 
 			<tr>
-				<?php $semaine = array('Lunae', 'Martis', 'Mercurii', 'Jovis', 'Veneris', 'Saturni', 'Dominicus');
+				<?php $semaine = array('','Lunae', 'Martis', 'Mercurii', 'Jovis', 'Veneris', 'Saturni', 'Dominicus');
 				for ($j=0; $j < count($semaine) ; $j++)
 					{ 
 				echo '<th><div>' . $semaine[$j] . '</div></th>';
@@ -29,24 +49,23 @@
 			</tr>
 				
 		<?php 
+			$semaine = date('W', strtotime('01-02-2015'));
 			$nombre_de_jours = cal_days_in_month(CAL_GREGORIAN, date('n'), date('Y'));
-			echo '<tr>';
-			$semaine = date('W');
+			echo '<tr><th>' . $semaine++ . '</th>';
 
 			$debut_de_mois = date('w', strtotime('01-' . date('n-Y'))-1);
-
 			
 			for ($i=0; $i < $debut_de_mois ; $i++) {
 
 				echo '<td></td>';
 			}
-
+			
+			$a=0;
 			$b=0;
-			for ($j=1; $j <= $nombre_de_jours ; $j++, $b++) { 
-				echo '<td> ' . $j . '</td>';
+			for ($j= 1; $j <= $nombre_de_jours ; $j++, $b++) { 
+				echo '<td> ' . chif_rome($j) . '</td>';
 				if ($b%7 == 0) {
-					echo '</tr><td>' . $semaine . '</td><tr>';
-					$semaine++;
+					echo '</tr><tr><th>0' . $semaine++ . '</th>';
 				}
 			}
 			echo '</tr>';
