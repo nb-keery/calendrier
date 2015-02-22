484c00a7715e55783228b7fe8a479b45e9bc663b
keery <mon-compte1131@live.fr>
2015-02-22 16:27:22 +0100

    Calendrier palier 5

--- calendrier.php
+++ calendrier.php
@@ -23,7 +23,7 @@
 
 
 		<?php 
-		if(isset($_GET['mois'])){
+			if(isset($_GET['mois'])){
 			if ($_GET['mois']== 0) {
 				$moisActuel = 12;
 			}
@@ -33,13 +33,41 @@
 			else { $moisActuel = $_GET['mois']; 
 			}
 		}
-		else { $moisActuel = date('n');} ?>
+		else { $moisActuel = date('n'); } ?>
 
-		<span><a href="?mois=<?php echo $moisActuel-1; ?>"><img src="gauche.png"></a> 
+		<?php
+		//Je crée une variable pour l'utiliser dans mon while plus bas
+		$z=2000;
 
-		<?php echo $mois[$moisActuel-1] . ' ' . date('Y'); ?>
+		//Si et seulement si j'appuie sur le bouton Comprobo alors l'anneeActuelle devient l'année que j'ai choisi dans la liste
+		if (isset($_POST['Comprobo'])) {
+			$anneeActuelle = $_POST['anneeChoisie'];
 
-		<a href="?mois=<?php echo $moisActuel+1; ?>"><img src="droite.png"></a></span>
+			//Et le mois se remet automatiquement sur le mois de janvier
+			$moisActuel = 1;
+			}
+		
+		// Si je n'appuie sur rien l'année affichée est l'année actuelle
+		else { $anneeActuelle = date('Y') ; } ?>
+
+		<span>
+			<a href="?mois=<?php echo $moisActuel-1; ?>"><img src="gauche.png"></a> 
+			<?php echo $mois[$moisActuel-1] .' ' . $anneeActuelle ; ?>
+			<a href="?mois=<?php echo $moisActuel+1; ?>"><img src="droite.png"></a>
+
+
+			<form action="" method="POST">
+				<select name="anneeChoisie">
+					<?php 
+					//Boucle qui sert a afficher dans un menu déroulant les années de 2000 jusqu'à l'année 2050
+					while ($z < 2050) {
+						$z++;
+						echo '<option>' . $z . '</option>';
+					} ?>
+				</select>
+				<input type="submit" name="Comprobo" value="Comprobo">
+			</form>
+		</span>
 
 		
 		<TABLE> 
@@ -54,13 +82,13 @@
 				
 		<?php 
 			//Calcul du numéro de la semaine (ex: Semaine 6) par rapport au premier du mois
-			$semaine = date('W', strtotime('01-' .  $moisActuel .'-' . date('Y')));
+			$semaine = date('W', strtotime('01-' .  $moisActuel .'-' . $anneeActuelle));
 
 			//Sert à connaitre le mois débute quel jour (ex: Mardi)
-			$debut_de_mois = date('w', strtotime('01-' . $moisActuel .'-' . date('Y'))-1);
+			$debut_de_mois = date('w', strtotime('01-' . $moisActuel .'-' . $anneeActuelle)-1);
 
 			//Calcul le nombre de jours de chaque mois
-			$nombre_de_jours = cal_days_in_month(CAL_GREGORIAN, $moisActuel, date('Y'));
+			$nombre_de_jours = cal_days_in_month(CAL_GREGORIAN, $moisActuel, $anneeActuelle);
 			
 			echo '<tr><th>' . $semaine++ . '</th>';
 			
@@ -77,7 +105,7 @@
 
 				//Si la variable $b est égale à 7 elle ferme la ligne du tableau et en ouvre une autre, et ainsi elle retourne à 0 pour créer plusieurs lignes
 				if ($i%7 == 0) {
-					echo '</tr><tr><th>0' . $semaine++ . '</th>';
+					echo '</tr><tr><th>' . $semaine++ . '</th>';
 				}
 			}
 			//On ferme la dernière ligne et termine le tableau
